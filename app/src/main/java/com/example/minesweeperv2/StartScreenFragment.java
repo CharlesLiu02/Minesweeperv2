05cb3d6b65fd28307ed86f4fd2f117c950e67b35
CharlesLiu02 <roomcool02@gmail.com>
2018-11-30 09:32:15 -0800

    added OnClickListeners for easy, medium, hard buttons

--- app/src/main/java/com/example/minesweeperv2/StartScreenFragment.java
+++ app/src/main/java/com/example/minesweeperv2/StartScreenFragment.java
@@ -10,23 +10,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class StartScreenFragment extends Fragment {
+public class StartScreenFragment extends Fragment{
+
+    Button buttonEasy;
+    Button buttonMedium;
+    Button buttonHard;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_start_screen, container, false);
 
-        Button button = rootView.findViewById(R.id.button_start_screen_easy);
+        buttonEasy = rootView.findViewById(R.id.button_start_screen_easy);
+        buttonMedium = rootView.findViewById(R.id.button_start_screen_medium);
+        buttonHard = rootView.findViewById(R.id.button_start_screen_hard);
+
         //replaces start screen fragment with minesweeper fragment when the button is clicked
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //opens minesweeper view
-                //replaces container main that holds start screen fragment with minesweeper fragment
-                FragmentManager fm = getFragmentManager();
-                fm.beginTransaction().replace(R.id.container_main, new MinesweeperFragment()).commit();
-            }
-        });
+        buttonEasy.setOnClickListener(new Listener());
+        buttonMedium.setOnClickListener(new Listener());
+        buttonHard.setOnClickListener(new Listener());
         return rootView;
     }
+
+    private class Listener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            //opens minesweeper view
+            //replaces container main that holds start screen fragment with minesweeper fragment
+            FragmentManager fm = getFragmentManager();
+            fm.beginTransaction().replace(R.id.container_main, new MinesweeperFragment()).commit();
+        }
+    }
+
 }
