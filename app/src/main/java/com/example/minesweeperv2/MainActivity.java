2de9a4bb5668fcbced8b1907bfbcb5ecbd29fa4a
Charles Liu <roomcool02@gmail.com>
2018-12-08 11:14:40 -0800

    created EndScreenDialog that opens on runtime, need to move method that opens the dialog to necessary spot created EndScreenDialog layout that needs to be formatted added comments to ensure readability

--- app/src/main/java/com/example/minesweeperv2/MainActivity.java
+++ app/src/main/java/com/example/minesweeperv2/MainActivity.java
@@ -2,6 +2,7 @@
 
 package com.example.minesweeperv2;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         //todo: get info about game done or not by using the checkGameDone() method in the MinsweeperFragment class
             //if method returns true then the endScreenDialog is shown
             //else do nothing
+
+        showResultsDialog();
+    }
+
+    private void showResultsDialog() {
+        //displaying end screen fragment
+        FragmentManager fm = getSupportFragmentManager();
+        //sets title of end screen fragment
+        EndScreenDialog endScreenDialog = EndScreenDialog.newInstance("End Game");
+        //calls the style to show title
+        endScreenDialog.setStyle(DialogFragment.STYLE_NORMAL, R.style.CustomDialog);
+        endScreenDialog.show(fm, "fragment_end_screen");
+
     }
 }
