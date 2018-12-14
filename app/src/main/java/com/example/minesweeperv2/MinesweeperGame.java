d6bba3c8e9737aa8a28bd2379dd6171dc5a9797a
JiaminXu1019 <jiaminxu1019@gmail.com>
2018-12-14 09:40:43 -0800

    Minesweeper game methods

--- app/src/main/java/com/example/minesweeperv2/MinesweeperGame.java
+++ app/src/main/java/com/example/minesweeperv2/MinesweeperGame.java
@@ -2,15 +2,19 @@
 
 package com.example.minesweeperv2;
 
+import android.graphics.Canvas;
+
 import java.sql.Array;
 
 public class MinesweeperGame {
     private Tile[][] array;
     private int numBombs;
+    private int canvasSize;
 
     public MinesweeperGame(int canvasSize, int numBombs) {
         array = new Tile[canvasSize][canvasSize];
         this.numBombs = numBombs;
+        this.canvasSize = canvasSize;
         for (int i = 0; i < canvasSize; i++) {
             for (int j = 0; j < canvasSize; j++) {
                 array[i][j] = new Tile(i, j);
@@ -31,7 +35,7 @@ public class MinesweeperGame {
             }
         }
 
-
+        // probability method
         // if (!array[i][j].ifHasBomb()) {
         //int x = (int) (Math.random() * canvasSize);
         // if (x == 1) {
@@ -45,70 +49,157 @@ public class MinesweeperGame {
     }
 
 
-    //Todo: make a checkGameDone() method
-    //checks if the game is done and prompts the endScreenDialog class in the MainActivity
-    //returns a boolean true or false depending on if game has ended or not
+    //checks if the game is lost and prompts the
 
+    public boolean gameWon() {
 
-    /* public boolean checkGameDone() {
+        boolean x = true;
 
-    }*/
+        for (int i = 0; i < canvasSize; i++) {
+            for (int j = 0; j < canvasSize; j++) {
 
+                if (array[i][j].ifHasBomb() == true) {
+                    if (array[i][j].ifHasFlag() == true) {
+                        x = true;
+                    } else {
+                        x = false;
+                    }
+                }
 
-    //TODO: create the flagTile method where user holds down and then the flag is drawn on tile
-    //if pressed time > 2 seconds && tile has no flag    then draw flag
-    //if pressed time > 2 seconds && tile has flag       then figure out way to remove the flag pic from the canvas
-
-    //if pressed time < 2 seconds                        then don't do anything
-
-    //TODO: create the logic for each tile in the onClick type method for a canvas
-    //check if tile has or doesn't have bomb then:
-    // logic for that
-    //if bomb:
-    //just draw the bomb pic on top and then prompt TheEndScreenFragment.java
-    //if not bomb, check surrounding tiles
-    //if surrounding tiles have no bombs then:
-    //draw a brown rectangle on top of the green one
-    //if not bomb, check surrounding tiles
-    //if surrounding tiles have bombs then:
-    //use calculateNumber(Tile tile) method (returns an int of #of bombs)
-    //just get num pic from drawables and draw the num pic on the tile
-
-
-    //Number(Tile tile) method to calculate the number image that should be displayed
-    //use the xLocation and yLocation of the tile variable passed in and get the tiles surrounding and call ifHasBomb to see if the surrounding tile has a bomb
-    //if there is a bomb in a surrounding tile, add a count variable
-    //return count
-
-    public int calculateNumber(Tile tile) {
-        int count = 0;
-
-        int xLocation = tile.getxLocation();
-        int yLocation = tile.getyLocation();
-
-        for (int i = xLocation - 1; i < xLocation + 2; i++) {
-            if (array[i][yLocation - 1].ifHasBomb()) {
-                count++;
             }
         }
 
-        for (int i = xLocation - 1; i < xLocation + 2; i++) {
-            if (array[i][yLocation + 1].ifHasBomb()) {
-                count++;
+
+            if (x == true){
+                return true;
+                // prompt victory fragment screen
+            } else {
+                return false;
+                //continue game
             }
         }
 
-        if (array[xLocation - 1][yLocation].ifHasBomb()) {
-            count++;
+
+
+
+        public void gameLost()
+        {
+            //prompt losing screen fragment
+
         }
 
-        if (array[xLocation + 1][yLocation].ifHasBomb()) {
-            count++;
+
+        //if flag is drawn on canvas then set same ifHasFlag to true on the tile with corresponding location
+        //if flag is undrawn on canvas then set same ifHasFlag to false on the tile with corresponding location
+
+        public void ifFlagDrawn ( int row, int col)
+        {
+            array[row][col].setHasFlag(true);
         }
 
-        return count;
+        public void ifFlagUndrawn ( int row, int col)
+        {
+            array[row][col].setHasFlag(false);
+        }
+
+
+        //check if tile has or doesn't have bomb then:
+        // logic for that
+        //if bomb:
+        //just draw the bomb pic on top and then prompt TheEndScreenFragment.java
+        //if not bomb, check surrounding tiles
+        //if surrounding tiles have no bombs then:
+        //draw a brown rectangle on top of the green one
+        //if not bomb, check surrounding tiles
+        //if surrounding tiles have bombs then:
+
+
+        public void onSingleTapClickReveal (int row, int col)
+        {
+
+            if (array[row][col].ifHasBomb() == true)
+            {
+                gameLost();
+            }
+            else if (array[row][col].ifHasBomb() == false) {
+                array[row][col].setNumber(calculateNumber(array[row][col]));
+                array[row][col].setRevealed(true);
+                {
+
+                    for (int i = col - 1; i < col + 2; i++) {
+                        if (calculateNumber(array[row - 1][i]) == 0)
+                        {
+                            array[row - 1][i].setRevealed(true);
+                            array[row - 1][i].setHasFlag(false);
+                            array[row - 1][i].setHasBomb(false);
+                        }
+                        else{
+                            array[row - 1][i].setRevealed(false);
+
+                        }
+                    }
+
+                    for (int i = col - 1; i < col + 2; i++) {
+                        if (array[row + 1][i].ifHasBomb()) {
+
+                        }
+                    }
+
+                    if (array[row][col - 1].ifHasBomb()) {
+
+                    }
+
+                    if (array[row][col + 1].ifHasBomb()) {
+
+                    }
+
+                }
+
+
+            }
+        }
+
+
+
+
+
+        //Number(Tile tile) method to calculate the number image that should be displayed
+        //use the xLocation and yLocation of the tile variable passed in and get the tiles surrounding and call ifHasBomb to see if the surrounding tile has a bomb
+        //if there is a bomb in a surrounding tile, add a count variable
+        //return count
+        //use calculateNumber(Tile tile) method (returns an int of #of bombs)
+        //just get num pic from drawables and draw the num pic on the tile
+
+        public int calculateNumber (Tile tile){
+            int count = 0;
+
+            int row  = tile.getRow();
+            int col = tile.getCol();
+
+            for (int i = col - 1; i < col + 2; i++) {
+                if (array[row - 1][i].ifHasBomb()) {
+                    count++;
+                }
+            }
+
+            for (int i = col - 1; i < col + 2; i++) {
+                if (array[row + 1][i].ifHasBomb()) {
+                    count++;
+                }
+            }
+
+            if (array[row][col - 1].ifHasBomb()) {
+                count++;
+            }
+
+            if (array[row][col + 1].ifHasBomb()) {
+                count++;
+            }
+
+            return count;
+        }
     }
-}
+
 
 
 
--- app/src/main/java/com/example/minesweeperv2/Tile.java
+++ app/src/main/java/com/example/minesweeperv2/Tile.java
@@ -2,33 +2,43 @@ package com.example.minesweeperv2;
 
 public class Tile {
 
-    private int xLocation, yLocation;
+    private int row, col;
     private boolean hasBomb;
     private boolean hasFlag = false;
     private boolean isRevealed = false;
+    private int number;
 
-        //constructor for the tile
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    //constructor for the tile
         //include hasBomb and hasFlag or not???
-    public Tile(int x, int y) {
-        this.xLocation = x;
-        this.yLocation = y;
+    public Tile(int row, int col) {
+        this.col = col;
+        this.row = row;
     }
 
     //getters and setters for the instance variables
-    public int getyLocation() {
-        return yLocation;
+
+    public int getRow() {
+        return row;
     }
 
-    public void setyLocation(int yLocation) {
-        this.yLocation = yLocation;
+    public void setRow(int row) {
+        this.row = row;
     }
 
-    public int getxLocation() {
-        return xLocation;
+    public int getCol() {
+        return col;
     }
 
-    public void setxLocation(int xLocation) {
-        this.xLocation = xLocation;
+    public void setCol(int col) {
+        this.col = col;
     }
 
     public boolean ifHasBomb() {
@@ -39,7 +49,7 @@ public class Tile {
         this.hasBomb = hasBomb;
     }
 
-    public boolean isHasFlag() { return hasFlag; }
+    public boolean ifHasFlag() { return hasFlag; }
 
     public void setHasFlag(boolean hasFlag) { this.hasFlag = hasFlag; }
 
